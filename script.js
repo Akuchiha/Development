21fc7a83f6b449b83f728c4924f8b6c65ec0b4ed
Arjunkumar <129575435+Akuchiha@users.noreply.github.com>
2025-02-24 11:29:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-function handleSubmit(event){
-	event.preventDefault();
-}
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const Name = document.getElementById('Name').value;
-    const Message = document.getElementById('message').value; 
-    
-    alert(`Name: ${Name}, Message: ${Message}`);
-    
-    console.log("contact form");
-    console.log("Name",name);
-    console.log("Message",message);
-});
+function handleSubmit() {
+    const name = document.getElementById('Name').value;
+    const message = document.getElementById('message').value;
 
+  
+    console.log('Name:', name);
+    console.log('Message:', message);
+
+    
+    document.getElementById('contactForm').reset();
+}
